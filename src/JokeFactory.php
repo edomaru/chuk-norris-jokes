387bf9596979e20b6758c891a67236d85e11e45d
Eding Muhamad <masaruedo@yahoo.com>
2020-11-30 17:55:44 +0700

    Test driven jokes

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -4,5 +4,23 @@ namespace Masaruedo\ChukNorrisJokes;
 
 class JokeFactory
 {
-    
+    protected $jokes = [
+        'Chuck Norris doesn\'t read books. He stares them down until he gets the information he wants.',
+        'Time waits for no man. Unless that man is Chuck Norris.',
+        'If you spell Chuck Norris in Scrabble, you win. Forever.',
+        'Chuck Norris does not sleep. He waits.',
+        'There is no chin behind Chuck Norris’ beard. There is only another fist.'
+    ];
+
+    public function __construct(array $jokes = [])
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
 }
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,12 +3,37 @@
 namespace Masaruedo\ChuckNorrisJokes\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Masaruedo\ChukNorrisJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
     {
-        $this->assertTrue(true);
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+        
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+    
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chukNorrisJokes = [
+            'Chuck Norris doesn\'t read books. He stares them down until he gets the information he wants.',
+            'Time waits for no man. Unless that man is Chuck Norris.',
+            'If you spell Chuck Norris in Scrabble, you win. Forever.',
+            'Chuck Norris does not sleep. He waits.',
+            'There is no chin behind Chuck Norris’ beard. There is only another fist.'
+        ];
+
+        $jokes = new JokeFactory();
+        
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chukNorrisJokes);
     }
 }
