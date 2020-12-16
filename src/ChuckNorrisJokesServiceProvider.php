88158038f838665ceab0dc3a967d1e068401b291
Eding Muhamad <masaruedo@yahoo.com>
2020-12-16 07:00:16 +0700

    update src/ChuckNorrisJokesServiceProvider.php

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace Masaruedo\ChukNorrisJokes;
 
+use Illuminate\Support\ServiceProvider;
+use Masaruedo\ChukNorrisJokes\JokeFactory;
+
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        dd('oye');
         $this->app->bind('chuck-norris', function () {
             return new JokeFactory();
         });
